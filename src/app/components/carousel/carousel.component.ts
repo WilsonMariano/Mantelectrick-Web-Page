d63aac350b0af56af7265aed3f3786b875726adb
wilsonmariano <mgw009@gmail.com>
2020-08-06 23:11:01 -0300

    Se sigue con carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-carousel',
@@ -10,6 +11,74 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	this.initCarousel();
+
+	// setTimeout(() => {
+	// 	$(window).stellar({
+	// 		responsive: true,
+	// 		parallaxBackgrounds: true,
+	// 		parallaxElements: true,
+	// 		horizontalScrolling: false,
+	// 		hideDistantElements: false,
+	// 		scrollProperty: 'scroll'
+	// 	  });
+		
+	// }, 2000);
+
+
+	$.Scrollax();
   }
+
+
+public initCarousel() {
+
+  var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+		$('.carousel-testimony').owlCarousel({
+			autoplay: true,
+			center: true,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 1
+				},
+				1000:{
+					items: 1
+				}
+			}
+		});
+
+	};
+	carousel();
+}
 
 }
