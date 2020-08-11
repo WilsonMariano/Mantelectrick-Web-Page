6ecf7404983872c606460ccb391a2ac2d49b9c60
wilsonmariano <mgw009@gmail.com>
2020-08-10 22:02:32 -0300

    Servidor de mensajeria

---
+++ server/contact.php
@@ -0,0 +1,28 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header('Content-type: application/json');
+    
+    $POST = file_get_contents("php://input");
+    $data = json_decode($POST);
+  
+    
+    $nombre = $data->nombre;
+    $email = $data->email;
+    $telefono = $data->telefono;
+    $asunto = $data->asunto;
+    $consulta = $data->mensaje;
+
+    $emailDestino = "info@mantelectrick.com";
+       
+
+    $mensaje = "E-mail: " . $email .PHP_EOL. "Nombre: " . $nombre .PHP_EOL. "Teléfono: " . $telefono .PHP_EOL. "Consulta: " . $consulta;
+
+    if(mail($emailDestino, $asunto, $mensaje)) {
+        http_response_code(200);
+        echo true;
+    } else {
+        http_response_code(500);
+        echo false;
+    }
+?>
